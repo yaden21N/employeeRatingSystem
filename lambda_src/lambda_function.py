ba027b4913b5eddae9fb712f0f1cbbda79680188
Yaden Naicker <yanaijhb025jhb@student.wethinkcode.co.za>
2026-09-23 00:03:47 +0200

    feat: require a Cognito JWT on the HTTP API

--- lambda_src/lambda_function.py
+++ lambda_src/lambda_function.py
@@ -139,7 +139,7 @@ def _json(status, data):
         "headers": {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Headers": "Content-Type,Authorization",
             "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
         },
         "body": json.dumps(data),
